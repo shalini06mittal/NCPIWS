fdcf567f3115e317b992f1500f365dc9111b495b
Shalini Mittal <shalini06.mittal@gmail.com>
2024-06-04 12:02:33 +0530

    stream api

--- CoreJavaWS/src/lambdas/TestStudent.java
+++ CoreJavaWS/src/lambdas/TestStudent.java
@@ -2,14 +2,35 @@ package lambdas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+
+interface Tester{
+	public <T> boolean test(T t);
+}
 public class TestStudent {
 
 	public static void printSpecialization(List<Student> list, Predicate<Student> predicate) {
 		for(Student student:list)
 			if(predicate.test(student))
 				System.out.println(student);
+	}
+	
+	public static void printDetails(List<Student> list, Consumer<Student> consumer) {
+		for(Student student:list)
+			consumer.accept(student);
+	}
+	public static List<String> getNames(List<Student> list, Function<Student, String> function) {
+		List<String> names = new ArrayList<>();
+		for(Student student:list)
+		{
+			String name = function.apply(student);
+			names.add(name);
+		}
+		return names;
 	}
 	public static void main(String[] args) {
 		
@@ -25,9 +46,48 @@ public class TestStudent {
 		listofstudents.add(new Student(999,"Jordan",58.6,"Finance"));
 		listofstudents.add(new Student(1010,"Chris",89.8,"Computer"));
 		
-		printSpecialization(listofstudents, student -> student.getSpecialization().equals("Mathematics"));
+		//1. stream of data to process -> stream do not store data
 		
-
+//		Stream<Student>  stream = listofstudents.stream();
+		
+		// intermediate
+//		Stream<Student>  filteredstream = stream.filter(
+//				student -> {
+//					System.out.println("in filter "+student.getName());
+//					return student.getSpecialization().equals("Mathematics");
+//					}
+//			);
+		//terminal
+//		filteredstream.forEach(student -> 
+//				System.out.println("for each "+student.getName()+" "+student.getPercentage()));
+//		
+//		listofstudents.stream()
+//		.filter(student -> student.getSpecialization().equals(""))
+//		.forEach(student -> 
+//		System.out.println("for each "+student.getName()+" "+student.getPercentage()));
+		//.forEach(System.out::println);
+		
+		
+		// 1 . stream
+		
+		Stream<Student>  stream = listofstudents.stream();
+		
+		//2. get the student object with names in uppercase 
+		Stream<Student> mapStudent = stream.map(student -> {
+			student.setName(student.getName().toUpperCase());
+			return student; 
+		});
+		//3. comsume
+		mapStudent
+		.filter(student -> student.getSpecialization().equals("Computer"))
+		.forEach(student -> 
+		System.out.println(student));
+		
+		
+//		printSpecialization(listofstudents, student -> student.getSpecialization().equals("Mathematics"));
+//		printDetails(listofstudents, student -> 
+//						System.out.println(student.getName()+" "+student.getPercentage()));
+//		getNames(listofstudents, student -> student.getName());
 	}
 
 }
