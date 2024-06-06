0e5adfb98ce705fe2f4901728ddedf874275d223
Shalini Mittal <shalini06.mittal@gmail.com>
2024-06-06 12:39:18 +0530

    stream api method reference

--- CoreJavaWS/src/methodref/MethodRefDemo.java
+++ CoreJavaWS/src/methodref/MethodRefDemo.java
@@ -1,6 +1,8 @@
 package methodref;
 
 import java.time.LocalDate;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -14,6 +16,36 @@ public class MethodRefDemo {
 
 	public static void main(String[] args) {
 		
+		Supplier<Integer> supp = () -> 10;
+		// Constructor
+//		String s1 = "Shalini";
+//		String s2 ="Mittal";
+//		Supplier<Person> persupp = ()-> new Person(s1,s2);
+//		Supplier<Person> persupp1= Person::new;
+//		
+//		System.out.println(persupp1.get());
+//		Function<String, Person> f1 = Person::new;
+//		System.out.println(f1.apply("Hey"));
+//		
+//		BiFunction<String, String, Person> bf1 = Person::new;
+//		System.out.println(bf1.apply("Hey","There"));
+//		
+		// static method reference
+//		Supplier<LocalDate> date = LocalDate::now;
+//		System.out.println(date.get());
+		
+		//Consumer<Person> con = person -> Person.entered(person);
+		Consumer<Person> con = Person::entered;
+		con.accept(new Person("A","B"));
+		
+		// instance method with an object of a class
+		Person p1 = new Person("a", "b");
+		Person p2 = new Person("c", "d");
+		p1.followed(p2);
+		
+		//Consumer<Person> f = p -> p.followed(p2);
+		Consumer<Person> f = p1::followed;
+		f.accept(p2);
 		
 	}
 
--- CoreJavaWS/src/methodref/Person.java
+++ CoreJavaWS/src/methodref/Person.java
@@ -2,33 +2,38 @@ package methodref;
 
 public class Person {
 
-	 String firstName;
-	    String lastName;
+	String firstName;
+	String lastName;
 
-	    Person() {
-	    	System.out.println("Person def");
-	    }
+	Person() {
+		System.out.println("Person def");
+	}
 
-	    Person(String firstName, String lastName) {
-	        this.firstName = firstName;
-	        System.out.println("Person param");
-	        this.lastName = lastName;
-	    }
-	   
-	   public static void entered( final Person person ) {
-			System.out.println( "Entered " + person.toString() );
-		}
-	   
-		public void followed( final Person another ) {
-			System.out.println( "Following the " + another.toString() );
-		}
+	Person(String firstName) {
+		this.firstName = firstName;
+		System.out.println("Person 1 param");
+		
+	}
+	Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		System.out.println("Person 2 param");
+		this.lastName = lastName;
+	}
 
-		public void exited() {  
-			System.out.println( "Exited " + this.toString() );
-		}
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return firstName+" : "+lastName;
-		}
+	public static void entered( final Person person ) {
+		System.out.println( "Entered " + person.toString() );
+	}
+
+	public void followed( final Person another ) {
+		System.out.println(this.toString()+ " \nFollowing the\n " + another.toString() );
+	}
+
+	public void exited() {  
+		System.out.println( "Exited " + this.toString() );
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return firstName+" : "+lastName;
+	}
 }
