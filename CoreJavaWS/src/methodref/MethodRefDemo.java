188d9853187d13221621d593cb1dbfadd96e9de4
Shalini Mittal <shalini06.mittal@gmail.com>
2024-06-07 10:33:38 +0530

    method reference

--- CoreJavaWS/src/methodref/MethodRefDemo.java
+++ CoreJavaWS/src/methodref/MethodRefDemo.java
@@ -18,21 +18,21 @@ public class MethodRefDemo {
 		
 		Supplier<Integer> supp = () -> 10;
 		// Constructor
-//		String s1 = "Shalini";
-//		String s2 ="Mittal";
-//		Supplier<Person> persupp = ()-> new Person(s1,s2);
-//		Supplier<Person> persupp1= Person::new;
-//		
-//		System.out.println(persupp1.get());
-//		Function<String, Person> f1 = Person::new;
-//		System.out.println(f1.apply("Hey"));
-//		
-//		BiFunction<String, String, Person> bf1 = Person::new;
-//		System.out.println(bf1.apply("Hey","There"));
-//		
+		String s1 = "Shalini";
+		String s2 ="Mittal";
+		Supplier<Person> persupp = ()-> new Person(s1,s2);
+		Supplier<Person> persupp1= Person::new;
+		
+		System.out.println(persupp1.get());
+		Function<String, Person> f1 = Person::new;
+		System.out.println(f1.apply("Hey"));
+		
+		BiFunction<String, String, Person> bf1 = Person::new;
+		System.out.println(bf1.apply("Hey","There"));
+		
 		// static method reference
-//		Supplier<LocalDate> date = LocalDate::now;
-//		System.out.println(date.get());
+		Supplier<LocalDate> date = LocalDate::now;
+		System.out.println(date.get());
 		
 		//Consumer<Person> con = person -> Person.entered(person);
 		Consumer<Person> con = Person::entered;
@@ -43,12 +43,17 @@ public class MethodRefDemo {
 		Person p22 = new Person("c", "d");
 		p1.followed(p22);
 		
-		Consumer<Person> f1 = p2 -> p22.followed(p2);
-		f1.accept(p22);
+//		Consumer<Person> f1 = p2 -> p22.followed(p2);
+//		f1.accept(p22);
 		System.out.println();
 		Consumer<Person> f = p22::followed;
 		f.accept(p1);
 		
+		// instance method with arbitrary object
+//		Consumer<Person> cc = person -> person.exited();
+		Consumer<Person> cc = Person::exited;
+		
+		cc.accept(new Person("Shalini"));
 	}
 
 }
