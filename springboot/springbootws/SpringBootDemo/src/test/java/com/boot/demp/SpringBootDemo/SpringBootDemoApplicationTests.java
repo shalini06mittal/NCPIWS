3bc013d552289e82eeda9ebd0295268e7d2bfc88
Shalini Mittal <shalini06.mittal@gmail.com>
2024-06-10 09:59:11 +0530

    assessment 2 and spring labs

---
+++ springboot/springbootws/SpringBootDemo/src/test/java/com/boot/demp/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.demp.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
