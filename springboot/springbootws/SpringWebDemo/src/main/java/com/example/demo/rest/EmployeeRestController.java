f1b6f4c372571b5aa12f44341ea24abfa42e9f52
Shalini Mittal <shalini06.mittal@gmail.com>
2024-06-21 11:48:12 +0530

    spring boot rest

--- springboot/springbootws/SpringWebDemo/src/main/java/com/example/demo/rest/EmployeeRestController.java
+++ springboot/springbootws/SpringWebDemo/src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
@@ -17,11 +20,17 @@ public class EmployeeRestController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	
+	//@RequestMapping(method = RequestMethod.GET)
 	@GetMapping()
 	public List<Employee> getAllEmployees()
 	{
 		return this.employeeService.getEmployees();
+	}
+	@GetMapping(path="/id")
+	public Employee getEmployeeById()
+	{
+		
+		return this.employeeService.getEmployeeById(0);
 	}
 }
 
